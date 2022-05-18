9fe8e25ba8378f88d54f37e0b705379715a3e0dc
João Victor Corado <jhonisilva545@gmail.com>
2022-05-18 17:26:14 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import { Jogos } from './types/Jogos'
 
 function App() {
+
+  const [jogos, setJogos] = useState<Jogos[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(()=>{
+    handleLoadButton();
+  },[]);
+
+  const handleLoadButton = () => {
+    setLoading(true)
+    fetch('https://api.rawg.io/api/platforms?key=c53ad660948b4344b61cb7214fb1b380').then((response)=>{
+      setLoading(false)
+      return response.json();
+      
+    })
+    .then(data => setJogos(data.results))
+    .catch((e) =>{
+    setLoading(false);
+    setJogos([]);
+    console.error(e);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {loading &&
+          <div className='load'>Carregando...</div>
+        }
+        {!loading && jogos.length > 0 &&
+          <>
+            <h1>Jogos</h1>
+            {jogos.map((item,index)=>(
+              <div className='card'>
+                  <img src={item.image_background}/>
+                  <h1>Plataforma:{item.name}</h1>
+                  <div key={index}>
+                    <h1>0s {item.games.length} jogos mais jogados:</h1>
+                    {item.games.map((item,index)=>(
+                      <div><h2>{item.name}</h2></div>))}
+                  </div>
+              </div>
+            ))}
+          </>
+        }
+        {!loading && jogos.length === 0 && 
+          <div>Tente novamente mais tarde</div>
+        }
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/types/Jogos.ts
@@ -0,0 +1,15 @@
+export type Jogos = {
+    id: number,
+    name: string,
+    slug: string,
+    image_background: string,
+    games:[
+        {
+            id: number,
+            name: string,
+            slug: string,
+        }
+    ]
+        
+    
+}
